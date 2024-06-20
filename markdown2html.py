7212948c77501a8e71b63006f5fba1d70e7e855a
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-06-20 00:35:56 -0700

    fifth commit for question 0

--- markdown2html.py
+++ markdown2html.py
@@ -7,14 +7,14 @@ import os
 
 
 if len(sys.argv) < 2:
-    print("Usage: ./markdown2html.py README.md README.html")
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
     sys.exit(1)
 
 markdown_file = sys.argv[1]
 html_file = sys.argv[2]
 
 if not os.path.exists(markdown_file):
-    print(f"Missing {markdown_file}", file=sys.stderr)
+    sys.stderr.write(f"Missing {markdown_file}\n")
     sys.exit(1)
 
 sys.exit(0)
